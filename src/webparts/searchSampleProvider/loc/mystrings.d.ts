c7a733c7a24e753a4cc1019415ce4f0e0d595142
chuck <chu11383@esri.com>
2023-01-30 09:48:39 -0800

    add sample search provider
    update ReadMe

---
+++ src/webparts/searchSampleProvider/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISearchSampleProviderWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SearchSampleProviderWebPartStrings' {
+  const strings: ISearchSampleProviderWebPartStrings;
+  export = strings;
+}
